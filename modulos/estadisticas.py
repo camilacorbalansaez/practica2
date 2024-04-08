07ea27e2c637dd41ebb4dd4c643b68edb3c90f6c
Camila Corbalan Saez <camila.sofi.cs@gmail.com>
2024-04-08 16:23:17 -0300

    inciso 2

--- modulos/estadisticas.py
+++ modulos/estadisticas.py
@@ -3,7 +3,3 @@ def generate (names, goals, goals_avoided, assists):
     for name, goal, goal_avoided, assist in zip(names, goals, goals_avoided, assists):
         statistics.append((name, goal, goal_avoided, assist))
     return statistics
-
-def know_player_goals(statistics):
-    for player in statistics:
-        print(f'El jugador ' ,player[0], ' anotó ' ,player[1], ' goles en la última temporada.')
