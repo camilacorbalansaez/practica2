721dbc63873cfde44d3f45a8b365331a6cb3d0d7
Camila Corbalan Saez <camila.sofi.cs@gmail.com>
2024-04-08 20:26:58 -0300

    cuarto inciso

--- modulos/funciones.py
+++ modulos/funciones.py
@@ -12,3 +12,8 @@ def most_influent(statistics):
     most_influent_player = max(statistics, key=lambda x: x[1] * 1.5 + x[2] * 1.25 + x[3])
     most_influent_total = most_influent_player[1] * 1.5 + most_influent_player[2] * 1.25 + most_influent_player[3]
     return most_influent_player[0], most_influent_total
+
+def calculate_scores_average(statistics, matches):
+    total_goals=sum(match[1] for match in statistics)
+    average=total_goals/matches
+    return average
