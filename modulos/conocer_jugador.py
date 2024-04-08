d4cf389ae6bec29085a1d3b3143934fd2aaeaf05
Camila Corbalan Saez <camila.sofi.cs@gmail.com>
2024-04-08 19:59:09 -0300

    modificaciones los primeros 3 incisos junto a los modulos

--- modulos/conocer_jugador.py
+++
@@ -1,3 +0,0 @@
-def know_player_goals(statistics):
-    for player in statistics:
-        print(f'El jugador {player[0]} anotó {player[1]} goles en la última temporada.')
--- modulos/estadisticas.py
+++
@@ -1,5 +0,0 @@
-def generate (names, goals, goals_avoided, assists):
-    statistics = []
-    for name, goal, goal_avoided, assist in zip(names, goals, goals_avoided, assists):
-        statistics.append((name, goal, goal_avoided, assist))
-    return statistics
