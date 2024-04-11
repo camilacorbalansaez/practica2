b4538a84b242b508124e21fb5d761b122609e566
Camila Corbalan Saez <camila.sofi.cs@gmail.com>
2024-04-11 18:21:20 -0300

    últimas modificaciones del módulo con las funciones

--- modulos/__init__.py
+++ modulos/__init__.py
@@ -3,9 +3,9 @@ def generate (names, goals, goals_avoided, assists):
     Genera una lista de tuplas con los datos que se pasan como argumentos.
 
     names= Corresponde a una lista de los nombres de los jugadores.
-    goals= Corresponde a una lista con la cantidad de goles realziados por los jugadores.
+    goals= Corresponde a una lista con la cantidad de goles realizados por los jugadores.
     goals_avoided= Corresponde a una lista con la cantidad de goles evitados por los jugadores.
-    assists= Corresponde a una lista con la cantidad de asistencias realziada por los jugadores. 
+    assists= Corresponde a una lista con la cantidad de asistencias realizadas por los jugadores. 
 
     Retorna la lista generada.
     
@@ -31,9 +31,9 @@ def most_influent(statistics):
     Busca al jugador más influyente calculando los goles realizados, goles evitados y asistencias según su valor.
     Los goles realizados valen 1.5 puntos, los goles evitados valen 1.25 puntos y las asistencias valen 1 punto. 
 
-    statistics= Corresponde a la lista de tuplas con las estadísticas de los jugadores realziada previamente. 
+    statistics= Corresponde a la lista de tuplas con las estadísticas de los jugadores realizada previamente. 
 
-    Retorna el nombre del jugador más influyente y la cantidad total de puntos
+    Retorna el nombre del jugador más influyente y la cantidad total de puntos.
     """
     most_influent_player = max(statistics, key=lambda x: x[1] * 1.5 + x[2] * 1.25 + x[3])
     most_influent_total = most_influent_player[1] * 1.5 + most_influent_player[2] * 1.25 + most_influent_player[3]
